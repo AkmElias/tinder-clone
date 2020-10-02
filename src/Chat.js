44ef7b68ab50f9a342be98148d19cb0cc92ef121
AkmElias <akmelias11@gmail.com>
2020-10-03 02:28:58 +0600

    chatScreen and send message done and a temporary finishing touch done

--- src/Chat.js
+++ src/Chat.js
@@ -1,16 +1,20 @@
 import React from "react";
 import "./Chat.css";
+import { Link } from "react-router-dom";
 import Avatar from "@material-ui/core/Avatar";
+
 function Chat({ name, message, timestamp, profilePic }) {
   return (
-    <div className="chat">
-      <Avatar className="chat__image" alt={name} src={profilePic} />
-      <div className="chat__details">
-        <h2>{name}</h2>
-        <p>{message}</p>
+    <Link to={`/chat/${name}`}>
+      <div className="chat">
+        <Avatar className="chat__image" alt={name} src={profilePic} />
+        <div className="chat__details">
+          <h2>{name}</h2>
+          <p>{message}</p>
+        </div>
+        <p className="chat__timestamp">{timestamp}</p>
       </div>
-      <p className="chat__timestamp">{timestamp}</p>
-    </div>
+    </Link>
   );
 }
 
